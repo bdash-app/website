4018339a19f5ad4e331be2796f565488c7dc8246
Kazuhito Hokamura <k.hokamura@gmail.com>
2018-05-15 23:39:21 +0900

    Implement download button

--- config.rb
+++ config.rb
@@ -3,6 +3,19 @@ set :layout, false
 config[:bdash_version] = `curl -s -I https://github.com/bdash-app/bdash/releases/latest | grep 'Location:'`.strip.slice(/[\d.]+\z/)
 
 helpers do
+  def platform_label_for(platform)
+    case platform
+    when :mac
+      "macOS"
+    when :windows
+      "Windows"
+    when :linux
+      "Linux"
+    else
+      raise ArgumentError, "Invalid platform: #{platform}"
+    end
+  end
+
   def download_url_for(platform)
     "https://github.com/bdash-app/bdash/releases/download/v#{bdash_version}/#{download_file_name_for(platform)}"
   end

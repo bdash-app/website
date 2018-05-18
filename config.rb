0cf94c43db6f5989a828f267a0cfb6e91fac82ff
Kazuhito Hokamura <k.hokamura@gmail.com>
2018-05-18 21:22:35 +0900

    Use fastly directly

--- config.rb
+++ config.rb
@@ -18,7 +18,7 @@ helpers do
   end
 
   def download_url_for(platform)
-    "/download/v#{bdash_version}/#{download_file_name_for(platform)}"
+    "https://bdash.global.ssl.fastly.net/v#{bdash_version}/#{download_file_name_for(platform)}"
   end
 
   def download_file_name_for(platform)
